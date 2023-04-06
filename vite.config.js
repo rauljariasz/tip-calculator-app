6a471f5e82443f880ffbb97c0dd27a37a6879910
raulariias <raularias24685@gmail.com>
2023-04-06 13:06:44 -0400

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    base: '/tip-calculator-app/'
-  },
-  plugins: [react()]
+  plugins: [react()],
+  base: '/tip-calculator-app/'
 })
